2cc8a4e04f040cfab4a229f9da6a3bdcd4967d71
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-04-09 15:42:01 -0300

    chore: add redis password

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -3,5 +3,7 @@ import { Redis } from "ioredis";
 export const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  password:
+    process.env.NODE_ENV === "production" ? process.env.REDIS_PASSWORD : "",
   tls: {},
 });
